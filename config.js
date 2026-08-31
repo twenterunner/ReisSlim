be4480ae1efb018bc784db4b7091bddb0efcdbaf
twenterunner <edwardbernhardi@gmail.com>
2026-08-31 22:31:15 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const VERSION = '2.2.1';
-export const BUILD = '2210';
+export const VERSION = '2.2.2';
+export const BUILD = '2220';
 export const ENGINE_VERSION = 200;
 export const STORAGE_SCHEMA_VERSION = 8;
 export const DATASET_VERSION = '2026.08.31.1';
